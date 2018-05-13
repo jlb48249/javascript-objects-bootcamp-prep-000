7b7d8d7d9411b33635207ddd920a2ddc9c5d3e81
Jacob Beckham <jacobbeckham44@gmail.com>
2018-05-13 21:06:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = { DotEXE: "Come Back to Me"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { [artistName]: songTitle })
+  playlist.artistName = songTitle;
+  return playlist
 }
